b72962bbb94be6047b363fd1c0b20a41142f8f19
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 15:40:37 +0530

    condense day 08

--- 08/p.py
+++ 08/p.py
@@ -1,12 +1,9 @@
-from sys import argv
 from math import lcm
-with open(argv[1] if len(argv) > 1 else 'input.txt') as f:
-    lines = [line.strip() for line in f.readlines()]
-direction,_, *maps, ld = *lines , len(lines[0])
-maps = {x.split(' = ')[0]:x.split(' = ')[1][1:-1].split(', ') for x in maps}
+dir, _, *maps = [line.strip() for line in open('input.txt').readlines()]
+maps={x.split(' = ')[0]:x.split(' = ')[1][1:-1].split(', ') for x in maps}
 def dist(p):
     i = ( i for i,_ in enumerate(iter(int,1)) )
     while set(p[p.count('A')//len(p)-1:]) - set('Z'):
-        p = maps[p][direction[next(i)%ld]=='R']
+        p = maps[p][dir[next(i)%len(dir)]=='R']
     return next(i)
 print(dist('AAA'), lcm(*map(dist, filter(lambda x: x.endswith('A'), maps))), sep='\n')
