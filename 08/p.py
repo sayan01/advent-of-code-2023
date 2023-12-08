8f0afed5dd8b89877d7b1fdfe0ef2f4eb0fd9232
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 15:51:46 +0530

    refactor day 08

--- 08/p.py
+++ 08/p.py
@@ -1,9 +1,9 @@
 from math import lcm
 dir, _, *maps = [line.strip() for line in open('input.txt').readlines()]
-maps={x.split(' = ')[0]:x.split(' = ')[1][1:-1].split(', ') for x in maps}
+maps={x.split(' = ')[0]:x.split(' = ')[1].strip('()').split(', ') for x in maps}
 def dist(p):
     i = ( i for i,_ in enumerate(iter(int,1)) )
     while set(p[p.count('A')//len(p)-1:]) - set('Z'):
         p = maps[p][dir[next(i)%len(dir)]=='R']
     return next(i)
-print(dist('AAA'), lcm(*map(dist, filter(lambda x: x.endswith('A'), maps))), sep='\n')
+print(dist('AAA'), lcm(*map(dist, [x for x in maps if x.endswith('A')]  )), sep='\n')
