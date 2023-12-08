9c8d6281fdec920add8897a4b69792beb68cfdc2
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 16:03:44 +0530

    add alternative to infinite index generator

--- 08/p.py
+++ 08/p.py
@@ -2,7 +2,7 @@ from math import lcm
 dir, _, *maps = [line.strip() for line in open('input.txt').readlines()]
 maps={x.split(' = ')[0]:x.split(' = ')[1].strip('()').split(', ') for x in maps}
 def dist(p):
-    i = ( i for i,_ in enumerate(iter(int,1)) )
+    i = ( i for i,_ in enumerate(iter(int,1)) ) # or __import__('itertools').count(0)
     while set(p[p.count('A')//len(p)-1:]) - set('Z'):
         p = maps[p][dir[next(i)%len(dir)]=='R']
     return next(i)
