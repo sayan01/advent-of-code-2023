d370ab884507fbd563b252c9cba3980252b38075
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 16:35:29 +0530

    refactor day 07 to be half the size

--- 07/p.py
+++ 07/p.py
@@ -1,16 +1,8 @@
-from sys import argv
-from collections import Counter as cnt
-from functools import partial as part
-
-input = argv[1] if len(argv) > 1 else "input.txt"
-
-with open(input) as f:
-    lines = [(x[0], int(x[1])) for x in [x.split() for x in f.readlines()]]
-
-def h(p, h):
-    cards, w, f = ('J' if p else '')+'23456789TJQKA', [51,42,32,33,23,24,15], cnt(h[0])
-    p and f.update({((f-cnt(J=5)).most_common(1) or [('K',0)])[0][0]:f.pop('J',0)})
+from collections import Counter as c
+lines = [(x[0], int(x[1])) for x in [x.split() for x in open('input.txt').readlines()]]
+def key(h):
+    f, j, w, cards = c(h[0]),c(J=5), [51,42,32,33,23,24,15], " J"[p]+'23456789TJQKA'
+    f.update({((f-j).most_common(1) or [('K',0)])[0][0]:f.pop('J',0)} if p else {})
     return [w.index(len(f)*10+f.most_common(1)[0][1])]+list(map(cards.find,h[0]))+[h[1]]
-
 for p in [0,1]:
-    print(sum([ x[0] * x[1][-1] for x in enumerate(sorted(lines,key=part(h, p)),1) ]))
+    print(sum([ x[0] * x[1][-1] for x in enumerate(sorted(lines,key=key),1) ]))
