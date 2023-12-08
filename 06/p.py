25f2d141c827f9de763ea8c81003ce8605e24a50
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 16:46:37 +0530

    condense day 06 to 06 lines

--- 06/p.py
+++ 06/p.py
@@ -1,20 +1,6 @@
-from sys import argv
-from math import sqrt, floor, ceil, prod, log10
-from functools import reduce
-input = argv[1] if len(argv) > 1 else "input.txt"
-with open(input) as ff:
-    lines = [line.strip() for line in ff.readlines()]
-
-times, distances = [list(map(int,line.split(':')[1].strip().split())) for line in lines]
-
-def findroots(t,d):
+from math import sqrt, floor, ceil, prod
+t, d = [list(map(int,line.split(':')[1].strip().split())) for line in open('input.txt')]
+def solve(t,d):
     return ceil((t + (disc:=(sqrt(t**2-4*d))))/2) - floor((t - disc)/2) - 1
-
-total = prod(map(findroots, times, distances))
-print(total)
-
-def intjoin(a,b):
-    return a*10**(floor(log10(b))+1)+b
-newtime = reduce(intjoin, times)
-newdist = reduce(intjoin, distances)
-print(findroots(newtime,newdist))
+print(prod(map(solve, t, d)))
+print(solve(int(''.join(map(str, t))), int(''.join(map(str, d)))))
