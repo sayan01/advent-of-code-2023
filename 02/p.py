80345072987c919da202534167020a0fe13ee18e
Sayan Ghosh <sgic2001@gmail.com>
2023-12-08 16:59:00 +0530

    condense day 02 to half size

--- 02/p.py
+++ 02/p.py
@@ -1,10 +1,5 @@
 from math import prod
-with open('input.txt') as f:
-    lines = [line.strip().replace(';', ',')[5:].split(': ') for line in f.readlines()]
-
-allowed = { 'red' : 12, 'green' : 13, 'blue' : 14 }
-lines = [[int(i[0]), dict(sorted([[j[1], int(j[0])] for j in [k.split(' ') for k in i[1].split(', ')]]))] for i in lines]
-
-part1 = sum([ i for i, line in lines if all([line[k] <= allowed[k] for k in line])])
-part2 = sum([ prod(line.values()) for _, line in lines])
-print(part1, part2, sep='\n')
+L = [x.strip().replace(';',',')[5:].split(': ') for x in open('input.txt')]
+L = [[int(i[0]),dict(sorted([[j[1],int(j[0])]for j in[k.split(' ')for k in i[1].split(', ')]]))]for i in L]
+p1 = sum([i for i,x in L if all([x[k]<={'red':12,'green':13,'blue':14}[k] for k in x])])
+print(p1, sum([ prod(line.values()) for _, line in L ]), sep='\n')
