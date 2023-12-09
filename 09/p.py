eed30bbc3ce6defa872d9c962810c49bdc178c0e
Sayan Ghosh <sgic2001@gmail.com>
2023-12-09 11:01:44 +0530

    minor changes

--- 09/p.py
+++ 09/p.py
@@ -1,5 +1,5 @@
 lines = [list(map(int,line.strip().split())) for line in open("input.txt")]
 def n(seq:list[int]) -> list[int]:
- diffs = [seq[i+1] - seq[i] for i in range(len(seq)-1)]
- return seq+[seq[-1]+diffs[0]] if set(diffs)=={0} else seq+[seq[-1]+n(diffs)[-1]]
+  d = [seq[i+1] - seq[i] for i in range(len(seq)-1)]
+  return seq+[seq[-1]+d[0]] if set(d)=={0} else seq+[seq[-1]+n(d)[-1]]
 [ print(sum([n(line[::p])[-1] for line in lines])) for p in [1,-1] ]
